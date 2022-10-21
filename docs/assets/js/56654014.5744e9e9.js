3d094f3e4c9bc7344576ba40c977f33fcbc3f0df
Tataklim <tatakaklim@gmail.com>
2022-10-21 10:54:42 +0300

    fix: add more test

---
+++ docs/assets/js/56654014.5744e9e9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1824],{6339:o=>{o.exports=JSON.parse('{"permalink":"/my-solana-docs2/blog/tags/chorusone","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
